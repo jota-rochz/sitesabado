bfae7503fed34edd8e3e8d80b7e379e4c0c6a72e
João Pedro <166226309+jota-rochz@users.noreply.github.com>
2024-05-11 14:08:33 -0300

    site pronto

--- js/script.js
+++ js/script.js
@@ -16,3 +16,9 @@ accordionHeaders.forEach(header =>{
         accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
     })
 })
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
